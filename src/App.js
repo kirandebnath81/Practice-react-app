5c49e223eadaf44a5d1df5df988bae5d7ea7fb40
Kiran Debnath <76046065+kirandebnath81@users.noreply.github.com>
2021-01-24 06:18:29 +0530

    docs: updated updated some documents

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Heo CodeSandbox</h1>
+      <h1>CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+      <h3>Welcome</h3>
     </div>
   );
 }
